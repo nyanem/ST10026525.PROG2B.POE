fb0dc4ec2e0bc4dd63cbba33c3dcb2900852294d
Khanyi Mabuza <131602333+nyanem@users.noreply.github.com>
2024-10-18 15:29:47 +0200

    Adding the database to the program.cs file

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ST10026525.PROG62112.POE.part1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Database builder service
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
